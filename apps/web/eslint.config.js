413ca9ff56e0b6e9692ddb46f4c82f11c004cb41
Justin Vietz <justin@vietz.dev>
2026-09-04 23:32:42 +0200

    feat(web): migrate app shell to Next.js

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,3 +1,6 @@
 import { config } from '@repo/eslint-config/index.js';
 
-export default [...config];
+export default [
+	{ ignores: ['src/routes/**', 'src/lib/components/**', 'src/lib/styles.ts', 'src/lib/index.ts'] },
+	...config
+];
