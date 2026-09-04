7eebfebf80b87d83796609ea0965f19d3d015697
Justin Vietz <justin@vietz.dev>
2026-09-05 01:57:15 +0200

    chore: remove legacy Svelte stack

--- apps/web/src/routes/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = ({ locals }) => {
-	return { user: locals.user };
-};
--- packages/ui/src/types.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A basic counter.
- */
-export type Counter = {
-  readonly count: number;
-  decrement(): void;
-  increment(): void;
-};
